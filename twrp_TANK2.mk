6a743b5a40932a82a482f1a89ee199edc1bd4086
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-08-29 21:51:09 +0100

    Update and rename twrp_LI7.mk to twrp_TANK2.mk

---
+++ twrp_TANK2.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from TANK2 device
+$(call inherit-product, device/8849/TANK2/device.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Product Specifics
+PRODUCT_NAME := twrp_TANK2
+PRODUCT_DEVICE := TANK2
+PRODUCT_BRAND := UNIHERTZ
+PRODUCT_MODEL := UNIHERTZ TANK2
+PRODUCT_MANUFACTURER := unihertz
+
+PRODUCT_GMS_CLIENTID_BASE := android-unihertz
