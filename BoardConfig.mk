e41446055c1d39f4be971c238bd8299510fb6725
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-08-29 21:33:07 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,20 +4,20 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/tecno/TECNO-LI7
+DEVICE_PATH := device/8849/TANK2
 
 # Inherit from mt6789-common
 include device/transsion/mt6789-common/BoardConfigCommon.mk
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := TECNO-LI7
+TARGET_OTA_ASSERT_DEVICE := 8849-TANK2
 
 # Init
-TARGET_INIT_VENDOR_LIB := libinit_TECNO-LI7
-TARGET_RECOVERY_DEVICE_MODULES := libinit_TECNO-LI7
+TARGET_INIT_VENDOR_LIB := libinit_TANK2
+TARGET_RECOVERY_DEVICE_MODULES := libinit_TANK2
 
 # TWRP Configs
-TW_DEVICE_VERSION := LI7_by_rama982
+TW_DEVICE_VERSION := TANK2 BY NINO
 
 # Brightness
 override TW_DEFAULT_BRIGHTNESS := 2047
